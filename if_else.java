de559daba82efb942a3a64bd11aeac46eaea52d1
FALAA-1285 <ahmadfalahi1285@gmail.com>
2024-09-16 20:51:00 +0700

    wri

---
+++ if_else.java
@@ -0,0 +1,22 @@
+public class if_else {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=30;
+
+        if (a<b){
+            if (b>c){
+                System.out.println("If 1&2 benar");
+            }
+                else{
+                    System.out.println("If 2 salah");
+                }
+                
+        }
+        
+        else{
+            System.out.println("if 1 salah");
+        }
+    }
+    
+}
---
+++ switch_case.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class switch_case {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int day;
+        System.out.print("Masukkan Hari ke?");
+        day=sc.nextInt();
+        
+
+        
+    }
+    
+}
